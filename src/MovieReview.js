a658a185a75050ea123facfd07c9586c0f57ee3e
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-08-03 11:27:26 +0300

    added movie review function

--- src/MovieReview.js
+++ src/MovieReview.js
@@ -1,1 +1,5 @@
 import React, { useState } from 'react';
+
+function MovieReview() {
+  const [formData, setFormData] = useState({ username: "", review: "" });
+  const [newData, setNewData] = useState([]);
