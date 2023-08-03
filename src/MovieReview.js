46117ad708f8b0265a28b27384d10f3c52dea5f5
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-08-03 11:28:24 +0300

    added Function to handle form submission

--- src/MovieReview.js
+++ src/MovieReview.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
 function MovieReview() {
-  const [formData, setFormData] = useState({ username: "", review: "" });
-  const [newData, setNewData] = useState([]);
+    const [formData, setFormData] = useState({ username: "", review: "" });
+    const [newData, setNewData] = useState([]);
+    
+    // Function to handle form submission
+    const handleSubmit = (event) => {
+    event.preventDefault();
