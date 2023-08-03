983f3e128c1e6946f9553cfe9cfd4efff4522208
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-08-03 11:30:11 +0300

    added on submit form

--- src/MovieReview.js
+++ src/MovieReview.js
@@ -11,4 +11,10 @@ function MovieReview() {
     // Adding the current form data to the newData array using the spread operator
     setNewData([...newData, formData]);
     setFormData({ username: "", review: "" }); // Clear form inputs after submission
-  }
+    }
+
+    return (
+    <div>
+      <form onSubmit={handleSubmit} className="review-form">
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
