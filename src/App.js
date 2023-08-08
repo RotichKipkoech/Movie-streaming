c833ddc7c0081d9e9026ddb4f1deda8fdc40bd6c
RotichKipkoech <132645931+RotichKipkoech@users.noreply.github.com>
2023-08-08 11:00:17 +0300

    Update App.js --rf router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./Dsiplay.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Main from "./Main";
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
     <Router>
